787f70a06413e4cc2065cd02dee49232c0859d24
Keagan Dickinson <keagantdickinson@gmail.com>
2022-07-01 15:28:47 +0200

    update

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const btns = document.querySelectorAll('.btn-dropdown');
+const btns = document.querySelectorAll(".question");
 
-btns.forEach(btn => {
-    btn.addEventListener('click', function(e) {
-      e.target.classList.toggle('open')
-    })
-  })
+function accordion() {
+  // this = the btn | icon & bg changed
+  this.classList.toggle("is-open");
+
+  // the acc-content
+  const content = this.nextElementSibling;
+  
+  // IF open, close | else open
+  if (content.style.maxHeight) content.style.maxHeight = null;
+  else content.style.maxHeight = content.scrollHeight  + "px";
+  
+}
+// event
+btns.forEach((el) => el.addEventListener("click", accordion));
